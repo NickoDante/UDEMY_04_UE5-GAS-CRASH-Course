dec01ea9724348325e4273e83e1338bc001510b6
NicoNietoTG <nnieto@teravisiongames.com>
2026-02-20 13:20:31 -0500

    21. Enemy Class

---
+++ Source/GAS_CrashCourse/Private/Characters/GCC_EnemyCharacter.cpp
@@ -0,0 +1,41 @@
+﻿// Nicolas Nieto - GCC - Copyright - 2026
+
+#include "Characters/GCC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+//----------------------------------------------------------------------------------------------------------------------
+AGCC_EnemyCharacter::AGCC_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+UAbilitySystemComponent* AGCC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void AGCC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (!IsValid(GetAbilitySystemComponent()))
+	{
+		return;
+	}
+	
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+	
+	if (!HasAuthority())
+	{
+		return;
+	}
+	
+	GiveStartupAbilities();
+}
---
+++ Source/GAS_CrashCourse/Public/Characters/GCC_EnemyCharacter.h
@@ -0,0 +1,30 @@
+﻿// Nicolas Nieto - GCC - Copyright - 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GCC_BaseCharacter.h"
+#include "GCC_EnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class GAS_CRASHCOURSE_API AGCC_EnemyCharacter : public AGCC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AGCC_EnemyCharacter();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+
+	virtual void BeginPlay() override;
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
