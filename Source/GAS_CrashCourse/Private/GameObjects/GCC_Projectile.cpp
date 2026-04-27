2841b616814502fe0601d74670e46fa0624d8ea3
NicoNietoTG <nnieto@teravisiongames.com>
2026-04-27 14:34:03 -0500

    58. Projectile Class

---
+++ Source/GAS_CrashCourse/Private/GameObjects/GCC_Projectile.cpp
@@ -0,0 +1,51 @@
+﻿// Nicolas Nieto - GCC - Copyright - 2026
+
+// Header Include
+#include "GameObjects/GCC_Projectile.h"
+
+// Engine includes
+#include "AbilitySystemComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Project includes
+#include "Characters/GCC_PlayerCharacter.h"
+
+//----------------------------------------------------------------------------------------------------------------------
+AGCC_Projectile::AGCC_Projectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
+	
+	bReplicates = true;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void AGCC_Projectile::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	AGCC_PlayerCharacter* PlayerCharacter = Cast<AGCC_PlayerCharacter>(OtherActor);
+	if (!IsValid(PlayerCharacter) || !PlayerCharacter->IsAlive())
+	{
+		return;
+	}
+	
+	UAbilitySystemComponent* AbilitySystemComponent = PlayerCharacter->GetAbilitySystemComponent();
+	if (!IsValid(AbilitySystemComponent) || !HasAuthority())
+	{
+		return;
+	}
+	
+	// Apply the Damage effect
+	FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.f, ContextHandle);
+	
+	// TODO: Use te damage variable for the ammount of damage to cause.
+	
+	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+
+	BP_SpawnImpactEffects();
+
+	Destroy();
+}
---
+++ Source/GAS_CrashCourse/Public/GameObjects/GCC_Projectile.h
@@ -0,0 +1,41 @@
+﻿// Nicolas Nieto - GCC - Copyright - 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GCC_Projectile.generated.h"
+
+class UGameplayEffect;
+class UProjectileMovementComponent;
+
+UCLASS()
+class GAS_CRASHCOURSE_API AGCC_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AGCC_Projectile();
+	
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	
+	UFUNCTION(BlueprintCallable)
+	float GetDamage() const { return Damage; }
+	
+protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Crash|Damage", meta = (ExposeOnSpawn, ClampMin = "0.0"))
+	float Damage{10.f};
+	
+	UFUNCTION(BlueprintImplementableEvent, Category = "Crash|Projectile")
+	void BP_SpawnImpactEffects();
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Crash|Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffect;	
+};
